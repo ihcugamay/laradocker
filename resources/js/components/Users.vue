7122cf9b1b55788f417f84fb9e99c5366379bbcc
ihcugamay <ymgc1994@icloud.com>
2019-03-07 22:01:02 +0900

    add Vue filter at User Table

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -22,15 +22,17 @@
                   <th>Name</th>
                   <th>Email</th>
                   <th>Type</th>
+                  <th>Registered At</th>
                   <th>Modify</th>
                 </tr>
-                <tr>
-                  <td>183</td>
-                  <td>John Doe</td>
-                  <td>11-7-2014</td>
-                  <td>
-                    <span class="tag tag-success">Approved</span>
-                  </td>
+
+                <tr v-for="user in users" :key="user.id">
+                  <td>{{user.id}}</td>
+                  <td>{{user.name}}</td>
+                  <td>{{user.email}}</td>
+                  <td>{{user.type | upText}}</td>
+                  <td>{{user.created_at |myDate}}</td>
+
                   <td>
                     <a href>
                       <i class="fa fa-edit blue"></i>
@@ -142,6 +144,7 @@
 export default {
   data() {
     return {
+      users: {},
       form: new Form({
         name: "",
         email: "",
@@ -153,12 +156,15 @@ export default {
     };
   },
   methods: {
+    loadUsers() {
+      axios.get("api/user").then(({ data }) => (this.users = data.data));
+    },
     createUser() {
       this.form.post("api/user");
     }
   },
-  mounted() {
-    console.log("Component mounted.");
+  created() {
+    this.loadUsers();
   }
 };
 </script>
